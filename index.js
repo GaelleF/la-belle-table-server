5448cff8d608803f1992df64698f5990731107ba
gaelleF <gaelle.fougere@outlook.fr>
2019-02-26 23:57:51 +0100

    upload pictures with multer

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-//import express from 'express';
-
 const express= require('express')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const util = require('util')
+const path = require('path')
+const fs = require('fs')
+
 const app = express()
+//const upload = util.promisify( multer({ dest : 'images/' }))
+const upload = multer({ dest : 'images/' })
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -9,10 +15,34 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extend: false }))
+
+// app.use('/images', express.static(publicImagesPath))
+
+
 app.get('/', (req, res) => {
   console.log('who ask me? ')
   return res.send('hello world!')  
 })
+
+app.post('/photos/upload', upload.single('newFile'),(req,res,next) => {
+  if (!req.file) {
+    console.log('request post fail', req.file)
+    return res.send('request fail')
+  }
+  const newFile = req.file
+  console.log('request post', req.file)
+  return res.send('request receive!') 
+})
+
+app.get('/photos', (req,res,next) => {
+ fs.readdir('images',(err,files) => {
+  console.log('FILES GET : ',files)
+  res.sendFile(__dirname+'/images/fac65ee74ab0ea6daef4e41490c1030d')
+ })
+})
+
 
 app.listen(5000, () => {
   console.log('server listen on 5000')
